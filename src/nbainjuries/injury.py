8c3d0d68b0b9fbe31b1a679205cc6a3b305ed38b
Srecko Skocilic <srecko.skocilic@gmail.com>
2026-02-11 07:49:00 +0100

    Added get_all_injuries method to retrieve a dataframe with all active injuries

--- src/nbainjuries/injury.py
+++ src/nbainjuries/injury.py
@@ -1,12 +1,21 @@
-from os import PathLike
 from datetime import datetime
+from os import PathLike
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 from . import _constants, _parser
 from ._exceptions import URLRetrievalError
 from ._util import _gen_url, _gen_filepath
 
 
-def get_reportdata(timestamp: datetime, local: bool = False, localdir: str | PathLike = None, return_df: bool = False,
-                   **kwargs):
+def get_reportdata(
+        timestamp: datetime, local: bool = False, localdir: str | PathLike = None, return_df: bool = False, **kwargs
+):
     """
     Extract injury data from the injury report at a specific date/time
     :param timestamp: datetime of the report for retrieval
@@ -16,23 +25,28 @@ def get_reportdata(timestamp: datetime, local: bool = False, localdir: str | Pat
     :param kwargs: custom headers to replace default
     """
     if not local:
-        headerparam = kwargs.get('headers', _constants.requestheaders)
+        headerparam = kwargs.get("headers", _constants.requestheaders)
     if timestamp < datetime(year=2023, month=5, day=2, hour=17, minute=30):  # 21-22 and part of 22-23 season
         area_bounds = _constants.area_params2223_a
         col_bounds = _constants.cols_params2223_a
-    elif datetime(year=2023, month=5, day=2, hour=17, minute=30) <= timestamp <= _constants.dictkeydts['2223'][
-        'ploffend']:  # remainder of 22-23 season
+    elif (
+            datetime(year=2023, month=5, day=2, hour=17, minute=30)
+            <= timestamp
+            <= _constants.dictkeydts["2223"]["ploffend"]
+    ):  # remainder of 22-23 season
         area_bounds = _constants.area_params2223_b
         col_bounds = _constants.cols_params2223_b
-    elif _constants.dictkeydts['2324']['regseastart'] <= timestamp <= _constants.dictkeydts['2324'][
-        'ploffend']:  # 23-24 season
+    elif (
+            _constants.dictkeydts["2324"]["regseastart"] <= timestamp <= _constants.dictkeydts["2324"]["ploffend"]
+    ):  # 23-24 season
         area_bounds = _constants.area_params2324
         col_bounds = _constants.cols_params2324
-    elif _constants.dictkeydts['2425']['regseastart'] <= timestamp <= _constants.dictkeydts['2425'][
-        'ploffend']:  # 24-25 season
+    elif (
+            _constants.dictkeydts["2425"]["regseastart"] <= timestamp <= _constants.dictkeydts["2425"]["ploffend"]
+    ):  # 24-25 season
         area_bounds = _constants.area_params2425
         col_bounds = _constants.cols_params2425
-    elif _constants.dictkeydts['2526']['regseastart'] <= timestamp:
+    elif _constants.dictkeydts["2526"]["regseastart"] <= timestamp:
         area_bounds = _constants.area_params2526
         col_bounds = _constants.cols_params2526
     else:  # out of range - default to 25-26 params
@@ -40,21 +54,22 @@ def get_reportdata(timestamp: datetime, local: bool = False, localdir: str | Pat
         col_bounds = _constants.cols_params2526
 
     if local:
-        df_result = _parser.extract_injreplocal(_gen_filepath(timestamp, localdir), area_headpg=area_bounds,
-                                                cols_headpg=col_bounds)
-        return df_result if return_df else df_result.to_json(orient='records', index=False, indent=2, force_ascii=False)
+        df_result = _parser.extract_injreplocal(
+            _gen_filepath(timestamp, localdir), area_headpg=area_bounds, cols_headpg=col_bounds
+        )
+        return df_result if return_df else df_result.to_json(orient="records", index=False, indent=2, force_ascii=False)
     else:
-        df_result = _parser.extract_injrepurl(gen_url(timestamp), area_headpg=area_bounds,
-                                              cols_headpg=col_bounds,
-                                              headers=headerparam)
-        return df_result if return_df else df_result.to_json(orient='records', index=False, indent=2, force_ascii=False)
+        df_result = _parser.extract_injrepurl(
+            gen_url(timestamp), area_headpg=area_bounds, cols_headpg=col_bounds, headers=headerparam
+        )
+        return df_result if return_df else df_result.to_json(orient="records", index=False, indent=2, force_ascii=False)
 
 
 def check_reportvalid(timestamp: datetime, **kwargs) -> bool:
     """
     Validate data availability of report at a specific date/time
     """
-    headerparam = kwargs.get('headers', _constants.requestheaders)
+    headerparam = kwargs.get("headers", _constants.requestheaders)
     try:
         _parser.validate_injrepurl(gen_url(timestamp), headers=headerparam)
         return True
@@ -65,7 +80,39 @@ def check_reportvalid(timestamp: datetime, **kwargs) -> bool:
 
 
 def gen_url(timestamp: datetime) -> str:
-    """
-    """
+    """ """
     return _gen_url(timestamp)
 
+
+def get_all_injuries() -> pd.DataFrame:
+    """
+    Returns a dataframe containing injury data for all injured players.
+    """
+    opts = Options()
+    opts.add_argument("--headless")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(options=opts, service=service)
+    driver.get(_constants.all_injuries_url)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    team_injuries = soup.find_all("div", class_="TableBaseWrapper")
+    injuries = []
+    for team in team_injuries:
+        team_name = team.find("div", class_="TeamLogoNameLockup-name").get_text(strip=True)
+        players = team.find_all("tr", class_="TableBase-bodyTr")
+        for player in players:
+            player_td = player.find_all("td", class_="TableBase-bodyTd")
+            player_data = {
+                "Team": team_name,
+                "Name": player.find("span", class_="CellPlayerName--long").get_text(strip=True),
+                "Updated": player.find("span", class_="CellGameDate").get_text(strip=True),
+                "Injury": player_td[3].get_text(strip=True),
+                "Status": player_td[4].get_text(strip=True),
+            }
+            injuries.append(player_data)
+    driver.quit()
+
+    return pd.DataFrame(injuries).sort_values("Team")
